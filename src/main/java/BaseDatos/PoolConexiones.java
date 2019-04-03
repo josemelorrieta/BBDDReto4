0ddedf67f6048569d25e84d8735d7d7984f80e11
José Manuel Muñoz Barragán <josemanuel.munozba@elorrieta-errekamari.com>
2019-04-03 22:50:35 +0200

    Fichero de configuracion de la Base de Datos

--- src/main/java/BaseDatos/PoolConexiones.java
+++ src/main/java/BaseDatos/PoolConexiones.java
@@ -1,27 +1,46 @@
 package BaseDatos;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.commons.dbcp2.BasicDataSourceFactory;
 
 public class PoolConexiones {
-	BasicDataSource basicDataSource = new BasicDataSource();
+	//BasicDataSource basicDataSource = new BasicDataSource();
+	DataSource dataSource;
 	
 	public DataSource CrearConexiones() {	
-		basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-		basicDataSource.setUsername("hr");
-		basicDataSource.setPassword("PepeJeans");
-		basicDataSource.setUrl("jdbc:mysql://localhost/termibus?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Europe/Madrid");
+//		basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//		basicDataSource.setUsername("hr");
+//		basicDataSource.setPassword("PepeJeans");
+//		basicDataSource.setUrl("jdbc:mysql://localhost/termibus?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=Europe/Madrid");
+		Properties propiedades = new Properties();
+		try {
+			propiedades.load(new FileInputStream("src/main/config/datasource_config.properties"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	
-		// Opcional. Sentencia SQL que le puede servir a BasicDataSource
-		// para comprobar que la conexion es correcta.
-		basicDataSource.setValidationQuery("select 1");
+		try {
+			dataSource = BasicDataSourceFactory.createDataSource(propiedades);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		return basicDataSource;
+		return dataSource;
 
 	}
 	

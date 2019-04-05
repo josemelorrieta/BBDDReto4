9ead35b6b01df5f5f395ea6ff21fa23cf9bd555a
IN1DM3B_10 <IN1DM3B_10@H015112.NET>
2019-04-05 14:19:05 +0200

    Refactor en clase consultaBD

--- src/main/java/BaseDatos/ConsultaBD.java
+++ src/main/java/BaseDatos/ConsultaBD.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import javax.sql.DataSource;
 
-public class ConsultaBBD {
+public class ConsultaBD {
 	
 	PoolConexiones pool = new PoolConexiones();
 	DataSource datasource;
--- src/main/java/programa/Principal.java
+++ src/main/java/programa/Principal.java
@@ -1,47 +1,19 @@
 package programa;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
-import BaseDatos.PoolConexiones;
+import BaseDatos.ConsultaBD;
 
 public class Principal {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		PoolConexiones pool = new PoolConexiones();
-		DataSource dataSource = pool.CrearConexiones();
-		Connection con = null;
-		ResultSet rs = null;
 		
-
-		try {
-			con = dataSource.getConnection();
-		} catch (SQLException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
+		ConsultaBD consulta = new ConsultaBD();
 		
-		rs = pool.generarConsulta(con, "SELECT nombre FROM hotel");
+		String query = "SELECT * FROM `hotel` WHERE 1";
 		
-		try {
-			while(rs.next()) {
-				System.out.println(rs.getString("nombre"));
-			}
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			try {
-				con.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		Object[] misDatos = consulta.consultarToArray(query);
+		
+		for(int i=0;i<misDatos.length;i++) {
+			System.out.println(misDatos[i].toString());
 		}
 
 	}

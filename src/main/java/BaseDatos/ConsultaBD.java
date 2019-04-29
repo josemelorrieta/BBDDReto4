ac7232e3bf77067b44bb8f7909325b075819c95b
YaroslavDemyanenko <yaroslav.demyanenko@elorrieta-errekamari.com>
2019-04-29 12:08:45 +0200

    Metodos login y registro actualizados

--- src/main/java/BaseDatos/ConsultaBD.java
+++ src/main/java/BaseDatos/ConsultaBD.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.sql.DataSource;
-import javax.swing.JOptionPane;
 
 public class ConsultaBD {
 
@@ -15,8 +14,11 @@ public class ConsultaBD {
 	DataSource datasource;
 	Connection con = null;
 
-	public String consultarToGson(String consulta) {
+	public ConsultaBD() {
 		datasource = pool.CrearConexiones();
+	}
+
+	public String consultarToGson(String consulta) {
 		String resultado = "[";
 		try {
 			con = datasource.getConnection();
@@ -40,7 +42,7 @@ public class ConsultaBD {
 							resultado += "},";
 					}
 				}
-				
+
 				return resultado.substring(0, resultado.length() - 1) + "]";
 			} else {
 				return "";
@@ -62,42 +64,23 @@ public class ConsultaBD {
 
 	/**
 	 * Insert generico a la base de datos
-	 * @param objetos array de Object, si se quiere meter un objeto se debe transformar en un array de Object, no pueden meterse matrizes, utilizar un for para eso
+	 * 
+	 * @param objetos     array de Object, si se quiere meter un objeto se debe
+	 *                    transformar en un array de Object, no pueden meterse
+	 *                    matrizes, utilizar un for para eso
 	 * @param nombreTabla nombre de la tabla a la cual se desea realizar un insert
 	 * 
 	 * @return booleano que indica si se han guardado bien los registros
 	 */
 	public boolean insertGenerico(Object[] objetos, String nombreTabla) {
-		datasource = pool.CrearConexiones();
 		try {
 			con = datasource.getConnection();
-			Class[] clasesObj = new Class[objetos.length];
-			String query = "insert into " + nombreTabla + " values(";
-
-			for (int i = 0; i < objetos.length; i++) {
-				clasesObj[i] = objetos[i].getClass();
-				query += "?,";
+			Class[] clasesObj = arrayClases(objetos);
+			String query = prepararQuery(objetos.length, nombreTabla);
+			PreparedStatement statementGenerico = generarStatement(objetos, clasesObj, query);
+			if (statementGenerico != null) {
+				statementGenerico.executeUpdate();
 			}
-
-			query = (query.substring(0, query.length() - 1)) + ");";
-
-			PreparedStatement statementGenerico = this.con.prepareStatement(query);
-			for (int i = 0; i < objetos.length; i++) {
-				if (clasesObj[i] == String.class) {
-					statementGenerico.setString(i + 1, (String) objetos[i]);
-				} else if (clasesObj[i] == Float.class) {
-					statementGenerico.setFloat(i + 1, (Float) objetos[i]);
-				} else if (clasesObj[i] == Double.class) {
-					statementGenerico.setDouble(i + 1, (Double) objetos[i]);
-				} else if (clasesObj[i] == Integer.class) {
-					statementGenerico.setInt(i + 1, (int) objetos[i]);
-				} else if (clasesObj[i] == java.util.Date.class) {
-					statementGenerico.setDate(i + 1, new java.sql.Date(((java.util.Date) objetos[i]).getTime()));
-				} else {
-					statementGenerico.setString(i + 1, (String) objetos[i]);
-				}
-			}
-			statementGenerico.executeUpdate();
 			return true;
 		} catch (SQLException e1) {
 			return false;
@@ -110,9 +93,42 @@ public class ConsultaBD {
 			}
 		}
 	}
-	
+
+	/**
+	 * Prepara el statement para su insercion a la base de datos organizando cada objeto de la forma que le corresponde
+	 * @param objetos array de Object
+	 * @param clases  array de clases, debe ser paralelo a objetos
+	 * @param query	  query que se quiere ejecutar
+	 * @return Prepared statement listo para ejecutar
+	 */
+	public PreparedStatement generarStatement(Object[] objetos, Class[] clases, String query) {
+		try {
+			PreparedStatement statementGenerico = this.con.prepareStatement(query);
+			for (int i = 0; i < objetos.length; i++) {
+				if (clases[i] == String.class) {
+					statementGenerico.setString(i + 1, (String) objetos[i]);
+				} else if (clases[i] == Float.class) {
+					statementGenerico.setFloat(i + 1, (Float) objetos[i]);
+				} else if (clases[i] == Double.class) {
+					statementGenerico.setDouble(i + 1, (Double) objetos[i]);
+				} else if (clases[i] == Integer.class) {
+					statementGenerico.setInt(i + 1, (int) objetos[i]);
+				} else if (clases[i] == java.util.Date.class) {
+					statementGenerico.setDate(i + 1, new java.sql.Date(((java.util.Date) objetos[i]).getTime()));
+				} else {
+					statementGenerico.setString(i + 1, (String) objetos[i]);
+				}
+				return statementGenerico;
+			}
+		} catch (SQLException e) {
+			return null;
+		}
+		return null;
+	}
+
 	/**
 	 * Crea un array de clases paralelo al array que se le pasa
+	 * 
 	 * @param objetos
 	 * @return array de clases
 	 */
@@ -122,5 +138,21 @@ public class ConsultaBD {
 			clasesObj[i] = objetos[i].getClass();
 		}
 		return clasesObj;
+	}
+
+	/**
+	 * Prepara el query
+	 * 
+	 * @param num
+	 * @param tabla
+	 * @return
+	 */
+	public String prepararQuery(int num, String tabla) {
+		String query = "insert into " + tabla + " values(";
+		for (int i = 0; i < num; i++) {
+			query += "?,";
+		}
+		query = (query.substring(0, query.length() - 1)) + ");";
+		return query;
 	}
 }
